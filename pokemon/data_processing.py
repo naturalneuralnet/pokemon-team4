a53fbc37b44dcd58c81464abf9e555f1a58186cd
Krzysztof <ch.sobczak97@gmail.com>
2025-04-29 12:55:51 +0200

    data processing files

---
+++ pokemon/data_processing.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+
+
+file_path = "pokemon.csv"
+
+
+def load_data(filepath):
+    """Load the Titanic dataset from a CSV file."""
+    return pd.read_csv(filepath)
+
+
+def clean_numerical_columns(df):
+    """Clean numerical columns by filling missing values."""
+    df["base_friendship"] = df["base_friendship"].fillna(df["base_friendship"].mode()[0])
+    df["base_experience"] = df["base_experience"].fillna(df["base_experience"].mode()[0])
+    df["percentage_male"] = df["percentage_male"].fillna(df["percentage_male"].mode()[0])
+    return df
+
+
+def clean_categorical_columns(df):
+    """Clean categorical columns by filling missing values."""
+    df["ability_2"] = df["ability_2"].fillna("None")
+    df["ability_hidden"] = df["ability_hidden"].fillna("None")
+    return df
+
+
+def drop_unnecessary_columns(df):
+    """Drop unnecessary columns and duplicates."""
+    df.drop(columns=["egg_type_number"], inplace=True)
+    df.drop(columns=["german_name"], inplace=True)
+    df.drop(columns=["japanese_name"], inplace=True)
+    df.drop(columns=["egg_cycles"], inplace=True)
+    df.drop_duplicates(inplace=True)
+    return df
+
+
+# def enrich_dataset(df):
+    """Add new features to enrich the dataset."""
+    df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+    bins = [-1, 12, 20, 40, 60, 100]
+    labels = ["Child", "Teen", "Young Adult", "Adult", "Senior"]
+    df["AgeGroup"] = pd.cut(df["Age"], bins=bins, labels=labels)
+    return df
+
+
+def load_and_clean_data(filepath):
+    """Load and clean the Pokemon dataset."""
+    df = load_data(filepath)
+    df = clean_numerical_columns(df)
+    df = clean_categorical_columns(df)
+    df = drop_unnecessary_columns(df)
+    # df = enrich_dataset(df)
+    return df
+
